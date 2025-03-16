fa30ea3b716fc29e5984c5b3695ce0fd33a86aca
PubaliDey <pubali_d.cse2020@msit.edu.in>
2025-03-16 17:24:26 +0530

    venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Pubali Dey",
+    author_email="deypubali2001@gmail.com",
+    packages=find_packages()
+)
